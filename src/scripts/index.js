53de4ab8c04f4b61926123287039114a576b9f94
Asaroz <77623721+Asaroz@users.noreply.github.com>
2021-06-24 13:25:04 +0200

    fin

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,19 @@
 import '../styles/main.scss';
 // Import any additional modules you want to include below \/
 
-
+var modal = document.getElementById("myModal");
+var closeModal = document.querySelector(".close");
 // \/ All of your javascript should go here \/
+closeModal.addEventListener("click",()=>{
+    modal.style.display ="none"
+})
 
+
+const modalProm = new Promise((resolve,reject)=>{
+    setTimeout(() => {
+        resolve()
+      },5000)
+})
+modalProm.then(()=>{
+    modal.style.display="block"
+})
